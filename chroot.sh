3aad1c71a7b7549318ab5151c2c2c9fdab2a99dd
Antônio Carlos <82233337+JuniorBecari10@users.noreply.github.com>
2025-07-31 20:56:59 -0300

    Add '--removable' flag in 'grub-install' in chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -48,5 +48,5 @@ chmod 440 /etc/sudoers.d/99-wheel
 systemctl enable NetworkManager
 
 # Set up GRUB bootloader for UEFI
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg

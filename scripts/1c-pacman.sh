d0654f710a1468ffc238ddf0e101d7e81e8474fb
Antônio Carlos <antonioocarlos@proton.me>
2025-10-18 13:16:35 -0300

    fix (i3, scripts): fix incompatibility issue with volumeicon and the newer version of gdk-pixbuf2, by using the older version

--- scripts/1c-pacman.sh
+++ scripts/1c-pacman.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 # List of packages to install
 PACKAGES=(
@@ -44,9 +45,30 @@ PACKAGES=(
     noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
     adw-gtk-theme
     flatpak
-    # lxappearance # disabled, may not be needed
-    # mpg123 # maybe pavucontrol needs it
 )
 
 # Install pacman packages
 pacman -Syu --noconfirm "${PACKAGES[@]}"
+
+# Workaround for volumeicon to work (install a legacy version of gdk-pixbuf2)
+# This will be the only package in the system that won't be upgraded.
+
+GDK_VERSION="2.42.12-2"
+GDK_PKG="gdk-pixbuf2-${GDK_VERSION}-x86_64.pkg.tar.zst"
+GDK_URL="https://archive.archlinux.org/packages/g/gdk-pixbuf2/${GDK_PKG}"
+
+# Download package if not already present
+if [ ! -f "$GDK_PKG" ]; then
+    curl -LO "$GDK_URL"
+fi
+
+# Install the legacy package
+sudo pacman -U --noconfirm "$GDK_PKG"
+
+# Prevent future updates from breaking volumeicon
+if ! grep -q "^IgnorePkg.*gdk-pixbuf2" /etc/pacman.conf; then
+    sudo sed -i '/\[options\]/a IgnorePkg = gdk-pixbuf2' /etc/pacman.conf
+fi
+
+# Clean up downloaded package
+rm -f "$GDK_PKG"

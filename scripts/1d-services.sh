d2b29b8276f946dd0cdf483a5a2cd5133dad0abb
Antônio Carlos <82233337+JuniorBecari10@users.noreply.github.com>
2025-12-02 14:21:42 -0300

    fix: add more service initializers for LightDM to correctly start

--- scripts/1d-services.sh
+++ scripts/1d-services.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+set -e
 
-# Enable networkmanager and lightdm services
+# Enable dbus and elogind
+ln -sf /etc/runit/sv/dbus /var/service/
+ln -sf /etc/runit/sv/elogind /var/service/
+
+# Enable networking and display manager
 ln -sf /etc/runit/sv/NetworkManager /var/service/
 ln -sf /etc/runit/sv/lightdm /var/service/

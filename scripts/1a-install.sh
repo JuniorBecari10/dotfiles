3755cc15c7703324f90f5973f52ca7e5704067e8
Antônio Carlos <antonioocarlos@proton.me>
2025-12-02 10:24:22 -0300

    minor: add 'yes' commands to ensure the installer and formatter don't ask the user for permission

--- scripts/1a-install.sh
+++ scripts/1a-install.sh
@@ -7,7 +7,7 @@
 # Set up filesystems
 mkfs.fat -F32 "$BOOT_PART"
 mkswap "$SWAP_PART"
-mkfs.ext4 "$MAIN_PART"
+yes | mkfs.ext4 "$MAIN_PART"
 
 # Mount partitions
 mount "$MAIN_PART" /mnt
@@ -16,18 +16,12 @@ swapon "$SWAP_PART"
 
 # Perform base system installation
 REPO="https://repo-default.voidlinux.org/current"
-xbps-install -Sy -y -R "$REPO" -r /mnt base-system linux linux-firmware git vim grub-x86_64-efi efibootmgr base-devel
-
-# Enable some services (test without them first)
-# ln -s /etc/runit/sv/dbus /mnt/var/service/
-# ln -s /etc/runit/sv/acpid /mnt/var/service/
-# ln -s /etc/runit/sv/udevd /mnt/var/service/
+yes | xbps-install -Sy -R "$REPO" -r /mnt base-system linux linux-firmware git vim grub-x86_64-efi efibootmgr base-devel
 
 # Generate fstab
 xgenfstab -U /mnt > /mnt/etc/fstab
 
-# Copy the general settings file into the installation, to keep it there for the user
-# The chroot script automatically deletes it.
+# Copy the general settings file into the installation; the chroot script automatically deletes it.
 cp config/general.sh /mnt
 chmod +x /mnt/general.sh
 

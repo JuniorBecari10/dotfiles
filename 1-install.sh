790063d5fe03abcb650f803698c7fa1d880a2a18
Antônio Carlos <antonioocarlos@proton.me>
2025-12-05 15:09:00 -0300

    feat: add error handling

--- 1-install.sh
+++ 1-install.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 # This should be run in the live CD.
+set -e
 
-./scripts/1a-install.sh
-echo "Done. You may reboot your computer."
+# Source settings
+. config/general.sh
+. config/passwords.sh
+
+if ./scripts/1a-install.sh; then
+    echo "(!) Installation completed successfully!"
+    echo "You may reboot your computer."
+else
+    umount -R /mnt
+    swapoff "$SWAP_PART"
+
+    echo "(X) Installation failed."
+    echo "Check the log above for errors, fix them, and then run the installer again."
+    echo "If you think this is an error in the script, please open an Issue."
+fi

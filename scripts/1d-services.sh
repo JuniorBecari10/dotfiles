8ffdafc08f77152ecc287300b82a634338f6d46c
Antônio Carlos <antonioocarlos@proton.me>
2025-12-09 11:14:46 -0300

    fix: fix 'device or resource busy' error

--- scripts/1d-services.sh
+++ scripts/1d-services.sh
@@ -5,10 +5,11 @@ set -e
 rm -rf /var/service
 ln -s /etc/runit/runsvdir/current /var/service
 
-# Fix broken symlink for DNS
-ln -sf /run/NetworkManager/resolv.conf /etc/resolv.conf
-
 # Enable services
 for svc in dbus elogind NetworkManager lightdm; do
     ln -snf "/etc/sv/$svc" /var/service/
 done
+
+# Fix broken symlink for DNS
+rm -rf /etc/resolv.conf
+ln -s /run/NetworkManager/resolv.conf /etc/resolv.conf

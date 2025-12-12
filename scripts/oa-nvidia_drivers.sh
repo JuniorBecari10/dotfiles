4b5d190049bd45233e6d6521342442f6e5c7a4d2
Antônio Carlos <82233337+JuniorBecari10@users.noreply.github.com>
2025-12-11 23:23:34 -0300

    remove: remove extra steps when installing NVIDIA drivers

--- scripts/oa-nvidia_drivers.sh
+++ scripts/oa-nvidia_drivers.sh
@@ -3,20 +3,18 @@ set -e
 
 PACKAGES="
 nvidia
-nvidia-dkms
-linux6.12-headers
 dkms
 "
 
 # Enable the nonfree package
 xbps-install -Sy void-repo-nonfree
 
-# Update repo index and install packages
+# Install packages
 xbps-install -Sy $PACKAGES
 
-# Fix issue with nouveau
-echo "blacklist nouveau options nouveau modeset=0" > /etc/modprobe.d/blacklist-nouveau.conf
-echo "options nvidia-drm modeset=1" > /etc/modprobe.d/nvidia-drm.conf
+# Fix issue with nouveau. Let's test it without this
+# echo "blacklist nouveau options nouveau modeset=0" > /etc/modprobe.d/blacklist-nouveau.conf
+# echo "options nvidia-drm modeset=1" > /etc/modprobe.d/nvidia-drm.conf
 
-# Regenerate initramfs
+# (Re)generate initramfs
 xbps-reconfigure -fa

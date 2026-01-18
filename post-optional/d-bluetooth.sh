656d936dd3163c1c8c442ce2c2daf27ec5ead171
Antônio Carlos <antonioocarlos@proton.me>
2026-01-18 19:41:04 -0300

    minor: correct the Bluetooth installation message

--- post-optional/d-bluetooth.sh
+++ post-optional/d-bluetooth.sh
@@ -2,4 +2,5 @@
 set -e
 
 sudo ~/dotfiles/scripts/odd-bluetooth.sh
-echo "Done. Logout of your session and log back in to apply the Bluetooth configurations."
+echo "Done. Reboot your system or restart 'dbus', then logout of your session and log back in to apply the Bluetooth configurations."
+echo "Just restarting is easier, btw."

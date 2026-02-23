335e2dd214431524400e6b3d8ed5dbd5f63901cc
Antônio Carlos <antonioocarlos@proton.me>
2026-02-22 21:30:28 -0300

    feat: add patch-i3 utility scripts

---
+++ utility/patch-i3-laptop.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+
+~/dotfiles/utility/patch-i3.sh
+~/dotfiles/scripts/ocbb-laptop_config.sh
---
+++ utility/patch-i3.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+
+cp -f ~/dotfiles/files/.config/i3/config ~/.config/i3/config
+cp -f ~/dotfiles/files/.config/i3blocks/i3blocks.conf ~/.config/i3blocks/i3blocks.conf
